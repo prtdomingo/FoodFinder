b9dfdfba0681b7c05f18b27e4d13292f71fbd06f
Philip Domingo <philiproimon.domingo@navitaire.com>
2018-01-12 19:10:08 +0800

    Added .env

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const builder = require('botbuilder');
 const restify = require('restify');
 const cognitiveServices = require('botbuilder-cognitiveservices');
